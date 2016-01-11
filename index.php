8a840661f0d8b77b519643a5c96052f3f082f2c3
Petr Chromec <petr.chromec@lmc.eu>
2016-01-11 23:43:02 +0100

    Psr4ClassLoader used

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once __DIR__ . '/vendor/Symfony/class-loader/Psr4ClassLoader.php';
+
+$loader = new \Symfony\Component\ClassLoader\Psr4ClassLoader();
+$loader->addPrefix('MF', __DIR__ . '/src');
+$loader->register();
+
 $app = new MF\App(new MF\Render());
 
 $app->renderResponse();
